18b547be32c5c114b47d0bb4169255c4d599c855
Kyle Hinton <kyle.hinton0@gmail.com>
2017-11-07 20:47:30 -0500

    Updated exception handling to Python 3 conventions, removed some crappy is not None nonsense, updated some random spelling and spacing.

--- alter/altertext.py
+++ alter/altertext.py
@@ -5,38 +5,37 @@ import fileinput
 def alterFile(filename,**kwargs):
     try:
         logfile = None
-        if kwargs['logfile'] != None:
+        if kwargs['logfile']:
             logfile = open(kwargs['logfile'], 'a')
         alterstring = 'Attempting to alter file {0}\n'.format(filename)
         print('\n'+alterstring)
-        if logfile != None:
+        if logfile:
             logfile.write(alterstring+'\n')
             
-        # Out put needs to be strored in a list due to how fileinput takes over
-        # the standard ouput
+        # Output needs to be stored in a list due to how fileinput takes over the standard output
             
         changeList = []
         linecount = 0
-        for line in fileinput.input(filename,inplace=True):
+        for line in fileinput.input(filename, inplace=True):
             linecount += 1
             f = line
             for ex in str(kwargs['alter']).split(','):
                 split = ex.split('->')
                 if split[0] in f:
                     if kwargs['verbose']:
-                        verboseline = 'Found term {0}, replacing with {1}'.format(split[0],split[1])
+                        verboseline = 'Found term {0}, replacing with {1}'.format(split[0], split[1])
                         changeList.append(verboseline)
-                    f = f.replace(split[0], split[1]).replace('\n','')
-                    changeList.append('Line {0}: {1}'.format(linecount,f))
+                    f = f.replace(split[0], split[1]).replace('\n', '')
+                    changeList.append('Line {0}: {1}'.format(linecount, f))
             print(f.strip('\n'))
         
         for outline in changeList:
             print(outline)
-            if logfile != None:
+            if logfile:
                 logfile.write(outline+'\n')
                 
-    except Exception, e:
-        print('\nAn exception has occured, please review the error and try again!\n\n{0}'.format(e))
+    except Exception as e:
+        print('\nAn exception has occurred, please review the error and try again!\n\n{0}'.format(e))
     finally:
         try:
             logfile.close()
--- search/jsonfile.py
+++ search/jsonfile.py
@@ -1,17 +1,18 @@
 import json
 
+
 def jSearchFile(filename,**kwargs):
     try:
         logfile = None
         filenameprinted = False
-        if kwargs['logfile'] is not None:
+        if kwargs['logfile']:
             logfile = open(kwargs['logfile'], 'a')
             
         with open(str(filename), 'r') as fil:
             searchstring = 'Searching file {0}\n'.format(filename)
             if kwargs['verbose']:
                 print('\n'+searchstring)
-                if logfile is not None:
+                if logfile:
                     logfile.write(searchstring+'\n')
                     filenameprinted = True
             linecount = 0
@@ -28,24 +29,24 @@ def jSearchFile(filename,**kwargs):
                     if pair[1] in decoded[pair[0]]:
                         if not filenameprinted:
                             print('\n' + searchstring)
-                            if logfile is not None:
+                            if logfile:
                                 logfile.write(searchstring + '\n')
                         if kwargs['verbose']:
                             verboseline = '\nSearchkey pair {0}:{1} found at'.format(pair[0],pair[1])
                             print(verboseline)
-                            if logfile is not None:
+                            if logfile:
                                 logfile.write(verboseline+'\n')
                         outline = 'Line {0}: '.format(linecount)
-                        if kwargs['outkeys'] is not None:
+                        if kwargs['outkeys']:
                             for u in kwargs['outkeys'].split(','):
                                 outline += '{0}:{1} '.format(u, decoded[u])
                         else:
                             outline += i
                         print(outline)
-                        if logfile is not None:
+                        if logfile:
                                 logfile.write(outline+'\n')
-    except Exception, e:
-        print('\nAn exception has occured, please review the error and try again!\n\n{0}'.format(e))
+    except Exception as e:
+        print('\nAn exception has occurred, please review the error and try again!\n\n{0}'.format(e))
     finally:
         try:
             logfile.close()
